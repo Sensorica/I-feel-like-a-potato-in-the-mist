b9f65cd3c7f639fd5229b355566642a180b9525f
snowy13 <jim.anastassiou@gmail.com>
2016-05-03 01:17:25 -0400

    added run_potato.py and test_potato.py

---
+++ run_potato.py
@@ -0,0 +1,38 @@
+#!/opt/local/bin/python2.4
+
+ #!/usr/bin/python
+          # -*- coding: latin-1 -*-
+import os, sys
+import serial
+import time
+from datetime import datetime
+
+#port = serial.Serial("/dev/tty.usbserial-A800ctKK", 115200)
+
+loremFile = open ('lorem.txt')
+lorem =  loremFile.read()
+loremLength = len(lorem)
+print loremLength
+maxLength = 50
+loremIndex = 0
+
+while loremIndex <= loremLength:
+    lineLength = maxLength
+    
+    while lorem[loremIndex + lineLength] != " ":
+	    lineLength -= 1
+    
+    printLine = lorem [loremIndex:loremIndex + lineLength] + '\n'	
+    print printLine + " " + lineLength
+    for c in printLine:
+        if c == '\n':
+            time.sleep(5)
+            port.write(c)
+            #print "new line"
+            time.sleep(5)
+        else:
+            time.sleep(0.6)
+            port.write(c)
+            print c
+    loremIndex += lineLength
+#port.close()
